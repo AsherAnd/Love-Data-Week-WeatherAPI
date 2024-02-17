94a6a403a9ee9bb56ca1b1ee0b6f93de54fa1250
AsherAnd <ashertew@gmail.com>
2024-02-17 10:33:51 -0500

    Added code, finished README and added requirements

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Love Data Week Workshop
+# Love Data Week Workshop - Open Data and Analysis with Python
+
+International Love Data Week is a global event held annually in February to raise awareness of the importance of research data management, sharing and reuse.
+
+On February 16th, I ran a workshop with the Toronto Metropolitan Library that instructed students with different coding experiences on using **Python** with Open Weather Map's **WeatherAPI** to make basic calls and demo a simple weather program.
+
+The program is a simple command line program that gives the user an option to either get the current weather of a specified city or get a weather forecast a specified city for 3 days.
+
+People who attended this workshop found this as not only a helpful way of learning how to work with APIs but also a way to start using Python.
--- main.py
+++ main.py
@@ -1,5 +1,68 @@
+import json
+import requests
+import os
+from dotenv import load_dotenv
+
+
+def get_forcast(api_key):
+    city = input("What city would you like to know the weather for?\n>>> ")
+
+    r = requests.get(
+        f"https://api.weatherapi.com/v1/forecast.json?key={api_key}&q={city}&days=3"
+    )
+
+    data = json.loads(r.text)
+    week = data["forecast"]["forecastday"]
+
+    print(f"Here is the weather for 3 days in {city}:")
+
+    for day in week:
+        print(
+            f"The high for {day['date']} is {day['day']['maxtemp_c']} degrees Celsius"
+        )
+        print(f"The low for {day['date']} is {day['day']['mintemp_c']} degrees Celsius")
+        print(f"The condition for {day['date']} is {day['day']['condition']['text']}")
+        print()
+
+    input()
+
+
+def get_current_weather(api_key):
+    city = input("What city would you like to know the weather for?\n>>> ")
+
+    r = requests.get(
+        f"https://api.weatherapi.com/v1/current.json?key={api_key}&q={city}"
+    )
+
+    data = json.loads(r.text)
+    location = data["location"]["name"]
+    temp_c = data["current"]["temp_c"]
+    condition = data["current"]["condition"]["text"]
+    feels = data["current"]["feelslike_c"]
+
+    print(
+        f"The weather in {location} is currently {temp_c} degrees but it feels like {feels} degrees, it's looking to be {condition}"
+    )
+
+    input()
+
+
 def main():
-    print("Love Data Week!")
+    load_dotenv()
+
+    while True:
+        api_key = os.getenv("KEY")
+
+        print("Welcome to Love Data Week!")
+        print("What would you like to do?")
+        print("1. Get Current Weather")
+        print("2. Get Weather Forecast")
+        option = input(">>> ")
+
+        if option == "1":
+            get_current_weather(api_key)
+        elif option == "2":
+            get_forcast(api_key)
 
 
 if __name__ == "__main__":
